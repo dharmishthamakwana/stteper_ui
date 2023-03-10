61e910e86475914afc41f17744f2220f3db3a54d
Admin <makwanadharmishtha44@gmail.com>
2023-03-10 21:36:33 +0530

    stapper_ui 10/03/23

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:stteper_ui/screen/home/provider/home_provider.dart';
 
 import 'screen/home/view/home_screen.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [ChangeNotifierProvider(create: (context) => HomeProvider(),)],
-      child: MaterialApp(debugShowCheckedModeBanner: false,
-      routes: {
-        '/':(context) => Homescreen(),
-      }),
-    ),
+    MaterialApp(debugShowCheckedModeBanner: false,
+    routes: {
+      '/':(context) => Homescreen(),
+    }),
   );
 }
